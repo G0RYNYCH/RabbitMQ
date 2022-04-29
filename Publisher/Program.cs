642b882f00e9e3b2cc5364a4a9d4e038400a232b
GORYNTCHdotNET <92940275+GORYNTCHdotNET@users.noreply.github.com>
2022-04-29 12:58:58 +0300

    FirstTry

---
+++ Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };//instance for conneting t server. "localhost" indicates that its working locally
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "dev-queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var message = "Hi from Publisher";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "dev-queue",
+                                     basicProperties: null,
+                                     body: body);
+
+                System.Console.WriteLine("Mess is sent into Default Ex");
+            }
+        }
+    }
+}
